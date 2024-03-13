8c34ef39356822a28db5f9028735188a8e7f9686
PheakdeySmith <pheakdeysmith@gmail.com>
2024-03-14 00:59:12 +0700

    complete room table

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -3,61 +3,45 @@
         <a href="#" class="sidebar-brand">
             Apsara<span><b>HOTEL</b></span>
         </a>
-        <div class="sidebar-toggler not-active">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
     </div>
     <div class="sidebar-body">
         <ul class="nav">
             <li class="nav-item nav-category">Main</li>
             <li class="nav-item">
-                <a href="{{ route('admin.dashboard') }}" class="nav-link{{ request()->routeIs('admin.dashboard') ? ' active' : '' }}">
+                <a href="{{ route('admin.dashboard') }}" class="nav-link">
                     <i class="link-icon" data-feather="box"></i>
                     <span class="link-title">Dashboard</span>
                 </a>
             </li>
-            <li class="nav-item nav-category">Fields</li>
+            <li class="nav-item nav-category">Customers</li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#customerPages" role="button"
-                    aria-expanded="false" aria-controls="customerPages">
-                    <i class="link-icon" data-feather="users"></i>
-                    <span class="link-title">Manage Customer</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
+                <a href="{{ route('admin.customertype.index') }}" class="nav-link">
+                    <i class="link-icon" data-feather="layout"></i>
+                    <span class="link-title">Customer Type</span>
                 </a>
-                <div class="collapse" id="customerPages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.customertype.index') }}" class="nav-link">Customer Type</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('admin.customer.index') }}" class="">Customer Table</a>
-                        </li>
-                    </ul>
-                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#roomPages" role="button"
-                    aria-expanded="false" aria-controls="roomPages">
-                    <i class="link-icon" data-feather="home"></i>
-                    <span class="link-title">Manage Room</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
+                <a href="{{ route('admin.customer.index') }}" class="nav-link">
+                    <i class="link-icon" data-feather="book-open"></i>
+                    <span class="link-title">Customer Table</span>
                 </a>
-                <div class="collapse" id="roomPages">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="{{ route('admin.roomtype.index') }}" class="nav-link">Room Type</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('admin.room.index') }}" class="nav-link">Room Table</a>
-                        </li>
-                    </ul>
-                </div>
+            </li>
+            <li class="nav-item nav-category">Rooms</li>
+            <li class="nav-item">
+                <a href="{{ route('admin.roomtype.index') }}" class="nav-link">
+                    <i class="link-icon" data-feather="layout"></i>
+                    <span class="link-title">Room Type</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.room.index') }}" class="nav-link">
+                    <i class="link-icon" data-feather="book-open"></i>
+                    <span class="link-title">Room Table</span>
+                </a>
             </li>
             <li class="nav-item nav-category">Transaction</li>
             <li class="nav-item">
-                <a href="{{ route('admin.reservation') }}" class="nav-link{{ request()->routeIs('admin.reservation') ? ' active' : '' }}">
+                <a href="{{ route('admin.reservation') }}" class="nav-link">
                     <i class="link-icon" data-feather="book-open"></i>
                     <span class="link-title">Reservation</span>
                 </a>
--- resources/views/admin/rooms/room/edit.blade.php
+++ resources/views/admin/rooms/room/edit.blade.php
@@ -6,63 +6,28 @@
     <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Edit Room</h4>
+                <h4 class="card-title">New Room</h4>
                 <form id="roomForm" class="form-inline" action="{{ route('admin.room.update', $rooms->room_id) }}" method="post">
                     @csrf
                     <div class="mb-3 row">
-                        <div class="col-md-12">
-                            <label for="roomCode" class="form-label">Code ID</label>
-                            <input id="roomCode" class="form-control" name="room_code" type="text" value="{{ $rooms->room_code }}">
+                        <div class="col-md-6">
+                            <label for="room_number" class="form-label">Room Number</label>
+                            <input id="room_number" class="form-control" name="room_number" type="text" value="{{ $rooms->room_number }}">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="room_name" class="form-label">Room Name</label>
+                            <input id="room_name" class="form-control" name="room_name" type="text" value="{{ $rooms->room_name }}">
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <div class="col-md-6">
-                            <label for="roomName" class="form-label">Name</label>
-                            <input id="roomName" class="form-control" name="room_name" type="text" value="{{ $rooms->room_name }}">
-                        </div>
-                        <div class="col-md-6">
-                            <label for="roomPhone" class="form-label">Phone Number</label>
-                            <input id="roomPhone" class="form-control" name="phone" type="tel" value="{{ $rooms->phone }}">
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <div class="form-group col-md-4">
-                            <label for="roomType">Type</label>
-                            <select id="roomType" class="form-control" name="roomtype_id">
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="roomtype_id" class="mb-2">Type</label>
+                            <select id="roomtype_id" class="form-control" name="roomtype_id">
+                                <option selected disabled value="">Choose...</option>
                                 @foreach ($room_types as $item)
+                                    <option value="{{ $item->roomtype_id }}">{{ $item->roomtype_name }}</option>
                                     <option value="{{ $item->roomtype_id }}" {{ $rooms->roomtype_id == $item->roomtype_id ? 'selected' : '' }}>{{ $item->roomtype_name }}</option>
                                 @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="roomDob">Date of Birth</label>
-                            <div class="input-group flatpickr" id="flatpickr-dob">
-                                <input type="text" class="form-control" id="roomDob" name="dob" value="{{ $rooms->dob }}">
-                                <span class="input-group-text input-group-addon" data-toggle><i data-feather="calendar"></i></span>
-                            </div>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label class="form-label">Gender</label>
-                            <div>
-                                <div class="form-check form-check-inline">
-                                    <input type="radio" class="form-check-input" name="sex" id="genderMale" value="M" {{ $rooms->sex == 'M' ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="genderMale">Male</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input type="radio" class="form-check-input" name="sex" id="genderFemale" value="F" {{ $rooms->sex == 'F' ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="genderFemale">Female</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3 row">
-                        <div class="form-group col-md-8">
-                            <label for="roomPassport">Passport Number</label>
-                            <input type="text" class="form-control" id="roomPassport" name="passportnumber" value="{{ $rooms->passportnumber }}">
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="roomCountry">Country</label>
-                            <select id="roomCountry" class="form-control" name="country">
                             </select>
                         </div>
                     </div>
@@ -75,3 +40,4 @@
 </div>
 
 @endsection
+
